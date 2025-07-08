8a01e05383fb16a0ed6baf9c20c766e426fdd8b5
rohitkumarrana <rohit.bca24@gmail.com>
2025-07-09 01:40:40 +0530

    Update johndoe.js

--- assets/js/johndoe.js
+++ assets/js/johndoe.js
@@ -1,1 +1,19 @@
+<script>
+$(document).ready(function(){
+    // Isotope initialization
+    var $grid = $('.portfolio-container').isotope({
+        itemSelector: '.col-md-6',
+        layoutMode: 'fitRows'
+    });
 
+    // Filter items on click
+    $('.portfolio .filters a').click(function(e){
+        e.preventDefault();
+        $('.portfolio .filters .active').removeClass('active');
+        $(this).addClass('active');
+
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+});
+</script>
