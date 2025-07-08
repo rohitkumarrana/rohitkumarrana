00530745d34e379b09cec174da4b44acb2b532e6
rohitkumarrana <rohit.bca24@gmail.com>
2025-07-09 01:23:26 +0530

    Update pdf.js

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
     $('#loadPdf').click(function () {
         // URL of the PDF file
-        var pdfUrl = '.resume_rohit.pdf'; // Change this to your PDF file path
+        var pdfUrl = 'resume_rohit.pdf'; // Change this to your PDF file path
         $('#pdfViewer').attr('src', pdfUrl).show();
     });
 });
